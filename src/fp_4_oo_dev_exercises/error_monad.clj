41d807ccad63bd026a9a4b02889f32541313dc67
Javier Holguera <javier.holguera@gmail.com>
2016-01-31 20:17:05 +0000

    Error monad exercise

---
+++ src/fp_4_oo_dev_exercises/error_monad.clj
@@ -0,0 +1,43 @@
+(require [clojure.algo.monads :refer :all])
+
+(def decider
+  (fn [step-value continuation]
+    (if (nil? step-value)
+      nil
+      (continuation step-value))))
+
+(def maybe-monad
+  (monad [m-result identity
+          m-bind decider]))
+
+(def oops!
+  (fn [reason & args]
+    (with-meta (merge {:reason reason}
+                      (apply hash-map args))
+      {:type :error})))
+
+(def oppsie?
+  (fn [value]
+    (= (type value) :error)))
+
+
+(defn error-detector [step-value continuation]
+  (if (oppsie? step-value)
+    step-value
+    (continuation step-value)))
+
+(def error-monad
+  (monad [m-result identity
+          m-bind error-detector]))
+
+(defn factorial [n]
+  (cond
+    (< n 0) (oops! "Factorial can never be less than zero." :number n)
+    (< n 2) 1
+    :else (* n (factorial (dec n)))))
+
+(defn result
+  (with-monad error-monad
+    (domonad [big-number (factorial -1)
+              even-bigger (* 2 big-number)]
+             (repeat :a even-bigger))))
